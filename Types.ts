3d330567a3d296b5a8c038602fcf75e1c400edc4
J Mor <jmortiger@gmail.com>
2026-03-18 09:44:39 -0400

    [UI] Added settings UI to change behavior.

--- Types.ts
+++ Types.ts
@@ -1,4 +1,5 @@
 import { DEBUG, DebugLevel, INFO, LOG, WARN } from "./DebugLevel";
+import { html } from "./HtmlTemplate";
 import { Direction, Point, RectInt, type IPoint } from "./Point2d";
 import Snake from "./Snake";
 
@@ -59,6 +60,8 @@ interface IEngineConfig extends ISnakeConfig {
   obstacleConfig:        IGridObjectConfig;
   millisecondsPerUpdate: number;
 };
+type _CfgPromiseConfig  = { form: HTMLFormElement; promise?: Promise<IEngineConfig>; defaults: IEngineConfig };
+type _CfgPromiseObjFull = { form: HTMLFormElement; promise: Promise<IEngineConfig>; defaults: IEngineConfig };
 class EngineConfig implements IEngineConfig {
   public constructor(
     public gridWidth: number,
@@ -163,6 +166,147 @@ class EngineConfig implements IEngineConfig {
     return (c.startingLength || 0) > 2
       && c.startingLength < NodeGeneration.MAX_GENERATED_LENGTH;
   }
+
+  // #region UI
+  // #region Construct from UI
+  // #region Helpers
+  private static inputType(element: HTMLElement) {
+    if (element instanceof HTMLInputElement) {
+      switch (element.type) {
+      case "number":
+      case "range":
+        return "number";
+
+      case "text":
+      case "url":
+        return "string";
+
+      case "radio":
+        return "enum";
+
+      default:
+        return "undefined";
+      }
+    }
+    if (element instanceof HTMLTextAreaElement)
+      return "string";
+    return "undefined";
+  }
+
+  private static inputTypeSpecific(element: HTMLElement) {
+    const v = this.inputType(element);
+    if (v !== "enum") return v;
+    return element.dataset["enum"];
+  }
+  // #endregion Helpers
+
+  public static fromFormAndData(form: HTMLFormElement, data: FormData, defaults = this.defaultConfig) {
+    const rv: any = {}; // eslint-disable-line @typescript-eslint/no-explicit-any
+    data.forEach((value, key) => {
+      let actingObj = rv, actingKey = key;
+      if (key.includes(".")) {
+        const keys = key.split(".");
+        for (let i = 0; i < keys.length - 1; actingObj = actingObj[keys[i++]!]) {
+          actingObj[keys[i]!] ||= {};
+        }
+        actingKey = keys.at(-1)!;
+      }
+      let parsedValue: any; // eslint-disable-line @typescript-eslint/no-explicit-any
+      const element = form.querySelector<HTMLElement>(`[name=${CSS.escape(key)}]${form.querySelector<HTMLInputElement>(`input[name=${CSS.escape(key)}][type=radio]`) ? `[value=${value}]` : ""}`)!, iType = this.inputType(element);
+      switch (iType) {
+      case "number":
+        parsedValue = Number(value);
+        break;
+      case "enum":
+        switch (this.inputTypeSpecific(element)) {
+        case "WallBehavior":
+          parsedValue = (value.toString().includes("wrap") || value.valueOf() == WallBehavior.wrap.valueOf()) ? WallBehavior.wrap : WallBehavior.endGame;
+          break;
+
+        case "string":
+        default:
+          parsedValue = value;
+          break;
+        }
+        break;
+
+      case "string":
+      default:
+        parsedValue = value;
+        break;
+      }
+      actingObj[actingKey] = parsedValue;
+    });
+    // TODO: Handle unchecked checkboxes (maybe w/ `form.elements` & `data.has`)?
+    return Object.assign({}, defaults, rv) as IEngineConfig;
+  }
+
+  public static fromFormDataEvent(e: FormDataEvent, defaults = this.defaultConfig) {
+    if (!(e.target instanceof HTMLFormElement)) {
+      console.warn("`FormDataEvent.target` is not an instance of `HTMLFormElement`");
+      return this.defaultConfig;
+    }
+    return this.fromFormAndData(e.target, e.formData, defaults);
+  }
+
+  public static fromSubmitEvent(e: SubmitEvent, defaults = this.defaultConfig) {
+    if (!(e.target instanceof HTMLFormElement)) {
+      console.warn("`FormDataEvent.target` is not an instance of `HTMLFormElement`");
+      return this.defaultConfig;
+    }
+    return this.fromFormAndData(e.target, new FormData(e.target, e.submitter), defaults);
+  }
+  // #endregion Construct from UI
+
+  private static refreshOnResolution(obj: _CfgPromiseConfig, onParsed?: (cfg: IEngineConfig) => void) {
+    const cb: (v: IEngineConfig) => void = (v) => {
+      obj.defaults = v;
+      obj.promise = this.createPromise(obj.form, obj.defaults, cb, onParsed);
+    };
+    obj.promise = this.createPromise(obj.form, obj.defaults, cb, onParsed);
+    return obj as _CfgPromiseObjFull;
+  }
+
+  private static createPromise(form: HTMLFormElement, defaults?: IEngineConfig, preResolution?: (v: IEngineConfig) => void, postResolution?: (v: IEngineConfig) => void) {
+    return new Promise<IEngineConfig>((resolve, _reject) => {
+      const listener: (this: HTMLFormElement, e: SubmitEvent) => void = (e) => {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+
+        // TODO: Validation
+        const newCfg = this.fromSubmitEvent(e, defaults) as IEngineConfig;
+        preResolution?.call(this, newCfg);
+        resolve(newCfg);
+        postResolution?.call(this, newCfg);
+        form.removeEventListener("submit", listener);
+      };
+      form.addEventListener("submit", listener);
+    });
+  }
+
+  public static toUI(c: IEngineConfig, onParsed?: (cfg: IEngineConfig) => void): _CfgPromiseObjFull {
+    const elem = html<HTMLFormElement>`
+    <form>
+      <label>Grid Width: <input type=number value=${c.gridWidth} name="gridWidth" /></label>
+      <label>Grid Height: <input type=number value=${c.gridHeight} name="gridHeight" /></label>
+      <label>Tick rate: <input type=number value=${c.millisecondsPerUpdate} name="millisecondsPerUpdate" /> milliseconds per update</label>
+      <span>
+        <h5>Pellets</h5>
+        <label>Starting: <input type=number value=${c.pelletConfig.startingObjs} name=pelletConfig.startingObjs /></label>
+        <label>Max: <input type=number value=${c.pelletConfig.maxObjs} name=pelletConfig.maxObjs /></label>
+      </span>
+      <span>
+        <h5>Obstacles</h5>
+        <label>Starting: <input type=number value=${c.obstacleConfig.startingObjs} name=obstacleConfig.startingObjs /></label>
+        <label>Max: <input type=number value=${c.obstacleConfig.maxObjs} name=obstacleConfig.maxObjs /></label>
+      </span>
+      <input type="submit" value="Start a new game with chosen settings" />
+    </form>
+    `;
+
+    return this.refreshOnResolution({ form: elem, defaults: c }, onParsed);
+  }
+  // #endregion UI
 };
 
 function randomIndex(a: Readonly<Array<unknown>>) {
--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import SnakeRenderer from "./SnakeRenderer";
+import { EngineConfig, type IEngineConfig } from "./Types";
 
 const canvas = document.createElement("canvas");
 canvas.width = 300;
@@ -6,17 +7,23 @@ canvas.height = 300;
 
 // canvas.style.imageRendering = "pixelated";
 document.body.prepend(canvas);
-const r = new SnakeRenderer(canvas);
+const state = EngineConfig.toUI(EngineConfig.defaults, initialize);
+function initialize(cfg: IEngineConfig) {
+  const r = new SnakeRenderer(canvas, cfg);
 
-// Wait for assets to load before setting up the game
-r.initGame().then(() => {
-  document.onkeyup = (e: KeyboardEvent) => {
-    if (e.key === " ") {
-      r.startGame();
-      document.onkeyup = null;
-    }
-  };
-  r.draw({ engine: r.engine });
-}).catch((error) => {
-  console.error("Failed to load game assets:", error);
-});
+  canvas.getContext("2d")?.clearRect(0, 0, canvas.width, canvas.height);
+  // Wait for assets to load before setting up the game
+  r.initGame().then(() => {
+    document.onkeyup = (e: KeyboardEvent) => {
+      if (e.key === " ") {
+        r.startGame();
+        document.onkeyup = null;
+      }
+    };
+    r.draw({ engine: r.engine });
+  }).catch((error) => {
+    console.error("Failed to load game assets:", error);
+  });
+}
+canvas.insertAdjacentElement("afterend", state.form); // document.body.appendChild(state.form);
+initialize(state.defaults);
